dc70aa1e8a6cefa148ec695baa2c5ade28819158
Igor Alyanoy <igoralyanoy@gmail.com>
2022-08-31 13:42:56 +0300

    refactor: remove console log

--- src/middlewares/isAuth.middleware.ts
+++ src/middlewares/isAuth.middleware.ts
@@ -13,18 +13,17 @@ async function isAuthMiddleware(
   try {
     const service = new AuthService();
     const token = req.context.token.slice(7);
-    console.log(
-      token ===
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZWE5N2Q1MmQ3MjlmMzI1ODdiYTYxYyIsImlhdCI6MTY1OTU0MTcyMiwiZXhwIjoxNjU5NTQ1MzIyfQ.4z8FBjXTD1pe4ShdrJOqxpF6mADnHMErP3KFkv8pFLo'
-    );
     const decodedToken = service.verifyToken(token);
     const user = await service.findUserById(decodedToken.id);
+
     if (!user) {
       return res.status(401).send({ auth: false, message: 'User not found.' });
     }
+
     if (user.token !== token) {
       return res.status(401).send({ auth: false, message: 'Invalid token.' });
     }
+
     req.context = {
       ...req.context,
       user,
